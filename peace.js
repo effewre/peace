18fa089ed36dc3d39ea194deaefc7f7506ecaeff
effewre <edgars.siklis@gmail.com>
2015-03-21 15:36:28 +0200

    f

--- peace.js
+++ peace.js
@@ -476,8 +476,10 @@
                     }
                 }
                 var newPosition = user.lastDC.position - afksRemoved;
-                if (newPosition <= 0) newPosition = 1;
-                var msg = subChat(basicBot.chat.valid, {name: basicBot.userUtilities.getUser(user).username, time: time, position: newPosition});
+                if (newPosition <= 0){ 
+				var msg = subChat(basicBot.chat.valid2, {name: basicBot.userUtilities.getUser(user).username, time: time});}
+                else {
+				var msg = subChat(basicBot.chat.valid, {name: basicBot.userUtilities.getUser(user).username, time: time, position: newPosition});}
                 basicBot.userUtilities.moveUser(user.id, newPosition, true);
                 return msg;
             }
