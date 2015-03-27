4ad5ed92e43f6a6cc9c628c48cd40c7703055c15
effewre <edgars.siklis@gmail.com>
2015-03-27 16:15:24 +0200

    d

--- peace.js
+++ peace.js
@@ -483,7 +483,8 @@
 				basicBot.userUtilities.moveUser(user.id, newPosition, true);
 				return msg;
 				}
-			},
+			}
+		},
 
         roomUtilities: {
             rankToNumber: function (rankString) {
