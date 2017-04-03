89fa0fa3b7babf55d105161a769b932ba983dc20
effewre <edgars.siklis@gmail.com>
2017-04-03 20:25:32 +0300

    sasa
    sasa

--- peace.js
+++ peace.js
@@ -2784,7 +2784,7 @@
 
 				skipCommand: {
                 command: ['skip'],
-                rank: 'bouncer',
+                rank: 'bouncer', 'suncis',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
